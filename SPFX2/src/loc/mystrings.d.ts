b173a93d382f8c59df172d885fe730473b22bfbb
LilMoonstar <nreya22@fpcoruna.afundacion.org>
2024-04-22 17:10:11 +0200

    AAL FIN

---
+++ SPFX2/src/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IGestorDeEventosWebpartWebPartStrings {
+    PropertyPaneDescription: string;
+    BasicGroupName: string;
+    DescriptionFieldLabel: string;
+    AppLocalEnvironmentSharePoint: string;
+    AppLocalEnvironmentTeams: string;
+    AppLocalEnvironmentOffice: string;
+    AppLocalEnvironmentOutlook: string;
+    AppSharePointEnvironment: string;
+    AppTeamsTabEnvironment: string;
+    AppOfficeEnvironment: string;
+    AppOutlookEnvironment: string;
+  }
+  
+  declare module 'GestorDeEventosWebpartWebPartStrings' {
+    const strings: IGestorDeEventosWebpartWebPartStrings;
+    export = strings;
+  }
+  
