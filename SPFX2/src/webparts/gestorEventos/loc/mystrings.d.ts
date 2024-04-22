0a9a03ccd919c41d936ebc989a46c8efd8d59d45
Rubén <rubencabo@comasis.com>
2024-04-22 09:36:43 +0200

    SPFX2

---
+++ SPFX2/src/webparts/gestorEventos/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IGestorEventosWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'GestorEventosWebPartStrings' {
+  const strings: IGestorEventosWebPartStrings;
+  export = strings;
+}
